446afe95c353c953078cd831bf4068e73ebe8b27
a14oscna <a14oscna@student.his.se>
2018-04-04 11:29:53 +0200

    I did it mom

--- app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
+++ app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
@@ -1,15 +1,22 @@
 package org.brohede.marcus.listviewapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
     private String[] mountainLocations = {"Alps","Alps","Alaska"};
-    private int[] mountainHeights ={4478,4808,6190};
+    private String[] mountainHeights ={"4478","4808","6190"};
     // Create ArrayLists from the raw data above and use these lists when populating your ListView.
 
 
@@ -18,6 +25,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        List<String> listData = new ArrayList<String>(Arrays.asList(mountainNames));
+        final List<String> listData2 = new ArrayList<String>(Arrays.asList(mountainLocations));
+        final List<String> listData3 = new ArrayList<String>(Arrays.asList(mountainHeights));
+
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),R.layout.list_item_textview,
+                R.id.my_item_textview, listData);
+
+        ListView myListView = (ListView)findViewById(R.id.my_listview);
+        myListView.setAdapter(adapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int show, long id) {
+                Context context = getApplicationContext();
+                CharSequence text = "Your chosen location: " + listData2.get(show) + ". The height is: " + listData3.get(show);
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
         // The onCreate method is run when the app is created.
         // Before you can implement this you need to create the layout xml files that
         // will hold/show your data created here. You need three create things:
